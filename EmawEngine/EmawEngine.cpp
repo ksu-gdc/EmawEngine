84797f3ea5a641a039b59edfb3069c73e6519808
maxerdwien <max@erdwien.com>
2015-02-12 16:32:12 -0600

    no change- test only

--- EmawEngine/EmawEngine.cpp
+++ EmawEngine/EmawEngine.cpp
@@ -134,7 +134,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_COMMAND	- process the application menu
 //  WM_PAINT	- Paint the main window
 //  WM_DESTROY	- post a quit message and return
-//
+// 
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
