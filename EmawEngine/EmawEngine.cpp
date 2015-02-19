3c109f5b3271c67407a5bb8bb909adda4e759295
James Blades Tyson Jr <linkmaster94@gmail.com>
2015-02-19 13:34:35 -0600

    removed a useless line

--- EmawEngine/EmawEngine.cpp
+++ EmawEngine/EmawEngine.cpp
@@ -71,7 +71,6 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 			fc.Update();
 			std::wstring test = fc.GetFps();
 			SetWindowText(hWnd, (LPCWSTR)&test[0]);
-			//SetWindowText(hWnd, L"Test");
 		}
 	}
 
