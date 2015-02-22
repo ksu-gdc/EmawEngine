fe59856c8ee4b8e1b5e383956546968d3fceedc4
Alex L'Esperance <alesperance22@gmail.com>
2015-02-21 22:53:04 -0600

    Changed TextureManager to Texture.  Also jumped thorugh a bunch of linker errors.  Will document

--- EmawEngine/stdafx.h
+++ EmawEngine/stdafx.h
@@ -20,5 +20,8 @@
 
 // TODO: reference additional headers your program requires here
 #include <d3d11.h>
+#include <d3dx11.h>
 
 #pragma comment(lib, "d3d11.lib")
+//
+#pragma comment(lib, "d3dx11.lib")
