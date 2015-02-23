95ec090b830822e0da5a4b36da6213c2c081cc63
Adr33 <andrew.kuzela@gmail.com>
2015-02-23 09:10:12 -0600

    AssetManager

--- EmawEngine/AssetManager.h
+++ EmawEngine/AssetManager.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //AssetManager for sound library
 //Author: Ondrej Kuzela
 
@@ -23,4 +24,44 @@ public:
 
 	//Method for adding filename to filename's map
 	bool add(string name, string filename);
+=======
+#pragma once
+#include <map>;
+#include "Asset.h"
+class AssetManager
+{
+private:
+	AssetManager();
+	~AssetManager();
+	// Statics
+	static bool instanceFlag;
+	static AssetManager *instance;
+
+	// Storage
+	std::map <std::string, Asset*> assets;
+	std::string rootAssetFolder;
+
+	// Loading
+	Asset* loadFromFile(std::string);
+	Asset* loadTexture(std::string);
+	Asset* loadShader(std::string);
+	Asset* loadMusic(std::string);
+	Asset* loadSoundFX(std::string);
+	Asset* loadVoxelMap(std::string);
+	Asset* loadBSPMap(std::string);
+
+	// Unloading
+	void unloadTexture(std::string);
+	void unloadShader(std::string);
+	void unloadMusic(std::string);
+	void unloadSoundFX(std::string);
+	void unloadVoxelMap(std::string);
+	void unloadBSPMap(std::string);
+
+public:
+	static AssetManager* getInstance();
+	void setRootFolder(std::string path);
+	Asset* load(std::string name);
+	void unloadAsset(std::string name);
+>>>>>>> origin/assetmanager
 };
