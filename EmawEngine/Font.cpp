7ac7a41385f3630fbb19ee30b2ce797fdef34cdd
Chris Delpire <chrisdelpire@gmail.com>
2015-02-24 11:39:49 -0600

    Added text block size calculation.
    Created a method to calculate the height and maximum width of a block of
    text. Also improved some comments.

--- EmawEngine/Font.cpp
+++ EmawEngine/Font.cpp
@@ -15,6 +15,13 @@ Font::~Font()
 {
 }
 
+// ---------------------------------------------------------------------
+// Loads a font asset by reading in a font file.
+// ---------------------------------------------------------------------
+// fontPath:	The path to the font file.
+// ---------------------------------------------------------------------
+// returns:		none.
+// ---------------------------------------------------------------------
 void Font::loadFont(string fontPath){
 
 	try{
@@ -22,8 +29,10 @@ void Font::loadFont(string fontPath){
 		ifstream fontInputStream;
 		fontInputStream.open(fontPath);
 
+		// Iterate through each line of the font file,
+		// parsing the lines until the all the necessary
+		// details of the font have been read and stored.
 		while (fontInputStream){
-
 			string line;
 			fontInputStream >> line;
 			ParseFontFile(line);
@@ -32,9 +41,15 @@ void Font::loadFont(string fontPath){
 	catch (exception e){
 		OutputDebugString(L"Error loading font asset.");
 	}
-
 }
 
+// ---------------------------------------------------------------------
+// Parses the font file, extracting all the information needed.
+// ---------------------------------------------------------------------
+// line:		The line to parse.
+// ---------------------------------------------------------------------
+// returns:		none.
+// ---------------------------------------------------------------------
 void Font::ParseFontFile(string line){
 
 	if (line.find("face=") != std::string::npos){
@@ -48,6 +63,10 @@ void Font::ParseFontFile(string line){
 	else if (line.find("file=") != std::string::npos){
 		int equalIndex = line.find("=");
 		pngPath = line.substr(equalIndex + 1, line.length() - 1);
+	}
+	else if (line.find("lineHeight=") != std::string::npos){
+		int equalIndex = line.find("=");
+		lineHeight = stoi(line.substr(equalIndex + 1, line.length() - 1));
 	}
 	else if (line.find("char") != std::string::npos){
 		loadingFontChar = new FontChar();
@@ -82,4 +101,48 @@ void Font::ParseFontFile(string line){
 		fontCharacters.insert(pair<int, FontChar*>(loadingFontChar->id, loadingFontChar));
 		loadingFontChar = NULL;
 	}
+}
+
+// ---------------------------------------------------------------------
+// Calculates the height and maximum width of a block of text.
+// ---------------------------------------------------------------------
+// text:		The text to calculate the height and max width of.
+// ---------------------------------------------------------------------
+// returns:		Returns a pair of ints <maximum width, height>.
+// ---------------------------------------------------------------------
+pair<int, int>* Font::textSize(string text){
+
+	int curWidth = 0;
+	int maxWidth = 0;
+	int line = 0;
+
+	for(char& c : text){
+
+		// If the first character exists, then we have atleast 1 line.
+		if (line == 0 && fontCharacters.count(c) == 1){
+			line = 1;
+		}
+
+		// If a new line character is found, then we are on a new line.
+		if (c == '\n'){
+			
+			line++;
+
+			// If the width of the current line is bigger than the max
+			// width, then set the max width to the current lines width.
+			if (curWidth > maxWidth){
+				maxWidth = curWidth;
+			}
+		}
+		else{
+
+			if (fontCharacters.count(c) == 1){
+				// Add the character's width to the current width.
+				curWidth += fontCharacters.at(c)->width;
+			}
+		}
+	}
+
+	return new pair<int, int>(maxWidth, line * lineHeight);
+
 }
--- EmawEngine/Font.h
+++ EmawEngine/Font.h
@@ -10,13 +10,15 @@ private:
 	std::map<int, FontChar*> fontCharacters;
 	void ParseFontFile(std::string line);
 	FontChar* loadingFontChar;
-public:
-	Font();
-	~Font();
 	std::string fontFace;
 	std::string pngPath;
 	int numOfChars;
+	int lineHeight;
+public:
+	Font();
+	~Font();
 
 	void loadFont(std::string);
+	pair<int, int>* Font::textSize(string line)
 	
 };
