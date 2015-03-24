b51dac12a85edd03120769d3811facc764523f30
Ninjtastic <ryanwoodburn92@gmail.com>
2015-03-23 21:42:42 -0500

    Created files that generate height values

---
+++ VoxelMapGen.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef VOXELMAPGEN_H
+#define VOXELMAPGEN_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <direct.h>
+
+class VoxelMapGen
+{
+public:
+	VoxelMapGen(string);
+	VoxelMapGen();
+	~VoxelMapGen();
+};
+#endif
