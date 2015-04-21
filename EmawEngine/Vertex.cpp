11435b0c406ce6362f5ded97ed31eb74890f2938
Alex L'Esperance <alesperance22@gmail.com>
2015-04-21 01:20:50 -0500

    Added vertex object (AND I ADDED THE FILES :) )

--- EmawEngine/Vertex.cpp
+++ EmawEngine/Vertex.cpp
@@ -2,11 +2,29 @@
 #include "Vertex.h"
 
 
-Vertex::Vertex()
+Vertex::Vertex(Vector3 position, Vector3 normal, Vector2 texture)
+{
+	this->Position = position;
+	this->Normal = normal;
+	this->Texture = texture;
+}
+
+
+Vertex::Vertex(f32 x, f32 y, f32 z, f32 nx, f32 ny, f32 nz, f32 u, f32 v) : 
+	Vertex(Vector3(x, y, z), Vector3(nx, ny, nz), Vector2(u, v))
 {
 }
 
 
-Vertex::~Vertex()
+VertexColor::VertexColor(Vector3 position, Vector3 normal, Vector2 textureCoord, 
+	ColorFloat vertexColor) : Vertex(position, normal, textureCoord)
+{
+	this->vertexColor = vertexColor;
+}
+
+VertexColor::VertexColor(f32 x, f32 y, f32 z, f32 nx, f32 ny, f32 nz, 
+	f32 u, f32 v, f32 r, f32 g, f32 b, f32 a) : 
+	VertexColor(Vector3(x, y, z), Vector3(nx, ny, nz), Vector2(u, v),
+	ColorFloat(r, g, b, a))
 {
 }
--- EmawEngine/Vertex.h
+++ EmawEngine/Vertex.h
@@ -1,10 +1,57 @@
 #pragma once
 
 
-class Vertex
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Color.h"
+
+enum VertexType
 {
-public:
-	Vertex();
-	~Vertex();
+	VertexStandard,
+	VertexColored,
+	VertexCount
 };
 
+struct Vertex
+{
+	Vector3 Position;
+	Vector3 Normal;
+	Vector2 Texture;
+
+	VertexType GetVertexType()
+	{
+		return VertexStandard;
+	}
+
+	Vertex(Vector3 position, Vector3 normal, Vector2 texture);
+	Vertex(f32 x = 0.f, f32 y = 0.f, f32 z = 0.f, 
+		f32 nx = 0.f, f32 ny = 0.f, f32 nz = 0.f, 
+		f32 u = 0.f, f32 v = 0.f);
+};
+
+struct VertexColor : public Vertex
+{
+	ColorFloat vertexColor;
+
+	VertexType GetVertexType()
+	{
+		return VertexColored;
+	}
+
+	VertexColor(Vector3 position, Vector3 normal, Vector2 texture, ColorFloat color);
+	VertexColor(f32 x = 0.f, f32 y = 0.f, f32 z = 0.f, 
+		f32 nx = 0.f, f32 ny = 0.f, f32 nz = 0.f, 
+		f32 u = 0.f, f32 v = 0.f, 
+		f32 r = 0.f, f32 g = 0.f, f32 b = 0.f, f32 a = 1.f);
+};
+
+inline u32 GetSize(VertexType type)
+{
+	if (type == VertexStandard)
+		return sizeof(Vertex);
+
+	if (type == VertexColored)
+		return sizeof(VertexColor);
+}
+
+void* GetLayout(VertexType type);
