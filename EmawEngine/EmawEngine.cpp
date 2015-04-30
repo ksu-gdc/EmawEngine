13ef59ae4986a06f2c7c129c87b91c6d86f8cfd1
max erdwien <max@erdwien.com>
2015-04-30 13:43:14 -0500

    finalized the example models

--- EmawEngine/EmawEngine.cpp
+++ EmawEngine/EmawEngine.cpp
@@ -101,9 +101,8 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 
 	bool do_cube = false;
 	bool do_pill = false;
-	bool do_monkey = true;
-	bool do_cat = false;
-	bool do_head = false;
+	bool do_monkey = false;
+	bool do_cat = true;
 	if (do_cube) {
 		Model* cube = new Model();
 		cube->load("models/obj-models/cube-tex.obj");
@@ -137,22 +136,12 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	if (do_cat) {
 		Model* cat = new Model();
 		cat->load("models/obj-models/cat.obj");
-		cat->LoadTexture(gdi.m_Device, "textures\\cat.png");
+		cat->LoadTexture(gdi.m_Device, "textures\\cat-flipped.png");
 
 		ModelNode* catNode = new ModelNode(cat);
 		catNode->setGraphicsDeviceInterface(&gdi);
 
 		root->addChild(catNode);
-	}
-	if (do_head) {
-		Model* head = new Model();
-		head->load("models/obj-models/head.OBJ");
-		head->LoadTexture(gdi.m_Device, "textures\\lambertian-flipped.jpg");
-
-		ModelNode* headNode = new ModelNode(head);
-		headNode->setGraphicsDeviceInterface(&gdi);
-
-		root->addChild(headNode);
 	}
 
 	//Controls the camera, WASD to move along the xz plane, Space and Ctrl to move up and down.
