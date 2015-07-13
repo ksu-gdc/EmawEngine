907e6dd91bceee7ee7d6b52656d078e22119e919
TheThirdWave <TheThirdWave@ksu.edu>
2015-07-13 09:26:51 -0500

    added case in ShaderAsset.load()
    there is now a special case for creating the inputlayout for a .geo
    shader.

--- EmawEngine/ShaderAsset.cpp
+++ EmawEngine/ShaderAsset.cpp
@@ -63,13 +63,25 @@ void* ShaderAsset::load(std::string str)
 
 	// changes to this structure must be reflected in the call to CreateInputLayout;
 	// the second parameter must equal the number of elements
+	if (type == GEO)
+	{
 	D3D11_INPUT_ELEMENT_DESC ied[] = {
 			{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 			{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-			{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
 	};
 
-	Interface->m_Device->CreateInputLayout(ied, 3, VS->GetBufferPointer(), VS->GetBufferSize(), &Shaders.InputLayout);
+	Interface->m_Device->CreateInputLayout(ied, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &Shaders.InputLayout);
+	}
+	else
+	{
+		D3D11_INPUT_ELEMENT_DESC ied[] = {
+				{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+				{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+				{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+		};
+
+		Interface->m_Device->CreateInputLayout(ied, 3, VS->GetBufferPointer(), VS->GetBufferSize(), &Shaders.InputLayout);
+	}
 	
 	return &Shaders;
 }
